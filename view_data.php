fc647e8798993cce61418d04eb5c5671156ca146
Ikhsan Maulana <61894676+akhsaul@users.noreply.github.com>
2021-12-02 11:31:40 +0700

    UPDATE implementation
    implement update, add jquery to get data

--- view_data.php
+++ view_data.php
@@ -1,14 +1,13 @@
-<?php
-require_once 'connection.php';
-$connection = connect(db: $dbname);
-$sql = "select id, username, email from $tbname;";
-$result = $connection->query($sql);
-?>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>View Data</title>
     <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"
+            integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13"
+            crossorigin="anonymous"></script>
     <style>
         body {
             text-align: unset !important;
@@ -21,39 +20,57 @@ $result = $connection->query($sql);
 </head>
 <body class="gradient">
 <div class="container">
+    <?php
+    require_once 'connection.php';
+    $connection = connect(db: $dbname);
+    $sql = "select * from $tbname;";
+    $result = $connection->query($sql);
+    ?>
     <div class="row justify-content-center">
-        <div class="col-md-9">
+        <div class="col-md-10">
             <div class="card shadow-lg o-hidden border-0 my-5">
                 <div class="card-body p-0 bg-gradient">
                     <div class="p-5">
                         <a class="btn btn-primary" href="index.html" role="button">Back to Home</a>
                         <div class="mb-3 position-relative table-responsive">
-                            <table class="table table-hover">
+                            <table class="table table-hover" style="text-align: center">
                                 <thead>
                                 <tr>
                                     <th scope="col" style="display: none;">id</th>
                                     <th scope="col">Username</th>
                                     <th scope="col">Email</th>
+                                    <th scope="col" colspan="2">Aksi</th>
                                 </tr>
                                 </thead>
                                 <tbody>
                                 <?php
-                                $id = -1;
                                 if ($result->num_rows > 0) {
                                     while ($row = $result->fetch_row()) {
-                                        $id = $row[0];
-                                        echo "<tr>";
-                                        for ($i = 0, $loopMax = count($row); $i < $loopMax; $i++) {
-                                            if ($i === 0) {
-                                                echo "<td style='display: none;'>$id</td>";
-                                            } else {
-                                                echo "<td>$row[$i]</td>";
-                                            }
-                                        }
-                                        echo "</tr>";
+                                        [$id, $username, $password, $email] = $row;
+                                        ?>
+                                        <tr>
+                                            <td style='display: none;'><?= $id ?></td>
+                                            <td><?= $username ?></td>
+                                            <td><?= $email ?></td>
+                                            <td>
+                                                <button type="button" class="btn btn-primary actUpdate"
+                                                        data-bs-toggle="modal" data-bs-target="#ubah"
+                                                        value="<?= $id ?>">Ubah
+                                                </button>
+                                            </td>
+                                        </tr>
+                                        <?php
                                     }
                                 } else {
-                                    echo "<tr><td style='display: none;'>$id</td><td>No Data</td><td>No Data</td></tr>";
+                                    ?>
+                                    <tr>
+                                        <td style='display: none;'>-1</td>
+                                        <td>No Data</td>
+                                        <td>No Data</td>
+                                        <td>No Action</td>
+                                        <td>No Action</td>
+                                    </tr>
+                                    <?php
                                 }
                                 ?>
                                 </tbody>
@@ -65,5 +82,79 @@ $result = $connection->query($sql);
         </div>
     </div>
 </div>
+<div class="modal fade del" id="ubah" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+     aria-labelledby="staticBackdropLabel">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <form class="user g-3 needs-validation" action="proses_data.php" method="post" novalidate>
+                <div class="modal-header">
+                    <h5 class="modal-title" id="staticBackdropLabelUpdate">Update Data</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <div class="mb-3 position-relative form-outline" style="display: none;">
+                        <label>DO
+                            <input type="text" name="do" required value="update">
+                        </label>
+                        <label>ID
+                            <input type="text" id="myId" name="id" required>
+                        </label>
+                    </div>
+                    <div class="mb-3 position-relative form-outline">
+                        <label for="username">Username</label>
+                        <input type="text" id="username" name="username" class="form-control form-control-user"
+                               placeholder="Username" required>
+                        <div class="invalid-tooltip">
+                            Contoh: John Smite
+                        </div>
+                    </div>
+                    <div class="mb-3 position-relative form-outline">
+                        <label for="password">Password</label>
+                        <input type="password" id="password" name="password" class="form-control form-control-user"
+                               placeholder="Password" minlength="8" required>
+                        <div class="invalid-tooltip">
+                            Kata Sandi minimal 8 huruf dan harus "[a-zA-Z0-9]"
+                        </div>
+                    </div>
+                    <div class="mb-3 position-relative form-outline">
+                        <label for="email">Email</label>
+                        <input type="email" id="email" name="email" class="form-control form-control-user"
+                               aria-describedby="emailHelp" placeholder="Email Address" required>
+                        <div class="invalid-tooltip">
+                            Contoh: admin@example.com
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Batal</button>
+                    <button type="submit" class="btn btn-primary d-block btn-user">Input</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+<script>
+    (() => {
+        "use strict"; // Start of use strict
+        $(document).ready(() => {
+            document.querySelectorAll(".actUpdate").forEach((e) => {
+                e.addEventListener('click', () => {
+                    $.post("proses_data.php", {
+                        do: "get",
+                        id: e.value,
+                        key: "all"
+                    }, (body) => {
+                        const data = body.split(",")
+                        document.getElementById("myId").value = data[0]
+                        document.getElementById("username").value = data[1]
+                        document.getElementById("password").value = data[2]
+                        document.getElementById("email").value = data[3]
+                    });
+                })
+            })
+        });
+    })(); // End of use strict
+</script>
+<script src="assets/custom/theme.js"></script>
 </body>
 </html>
