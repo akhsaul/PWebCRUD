23d759d8df7805fdcc2f25a94efe5331adc4f2cb
Ikhsan Maulana <61894676+akhsaul@users.noreply.github.com>
2021-12-02 12:08:16 +0700

    data process
    add "get" to get some data, add "update" to update data

---
+++ proses_data.php
@@ -0,0 +1,41 @@
+<?php
+require_once 'connection.php';
+$id = $_POST['id'];
+$todo = $_POST['do'];
+if ($todo === "get") {
+    $what = $_POST['key'];
+    $connection = connect(db: $dbname, debug: false);
+    $sql = "";
+    if ($what === "all") {
+        $sql = "select * from $tbname where id = $id";
+    } else if ($what === "username") {
+        $sql = "select username from $tbname where id = $id";
+    } else {
+        $connection->close();
+        error("view_data.php", "Unknown Request", "Wrong Request From view data page", "id = $id, do = $todo");
+    }
+    $result = $connection->query($sql);
+    if ($result) {
+        echo implode(",", $result->fetch_row());
+    }
+} else {
+    $connection = connect(db: $dbname);
+    if ($todo === "update") {
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        $email = $_POST['email'];
+        $sql = "update";
+        $result = $connection->query($sql);
+        if ($result) {
+            echoToConsole("Success Update Data");
+            move("view_data.php");
+        } else {
+            $msg = $connection->connect_error .= "id = $id, do = $todo";
+            $connection->close();
+            error("view_data.php", "Failed Update Data", $msg);
+        }
+    } else {
+        $connection->close();
+        error("view_data.php", "Unknown Request", "Wrong Request From view data page", "id = $id, do = $todo");
+    }
+}
